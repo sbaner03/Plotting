ed8491cf76443e6615c3b3396dfd08c48c51889c
Supratim Banerjee <sbanerjee12@gmail.com>
2017-08-04 12:38:58 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import createPlotlyComponent from 'react-plotlyjs';
+//See the list of possible plotly bundles at https://github.com/plotly/plotly.js/blob/master/dist/README.md#partial-bundles or roll your own 
+var Plotly = require('plotly.js')
+const PlotlyComponent = createPlotlyComponent(Plotly);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
